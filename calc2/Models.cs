986281e0df6c8189fef40be387382917e7d0a3ea
Sofyan Ardianto <sofyanard@gmail.com>
2023-01-09 16:25:20 +0700

    Add project files.

---
+++ calc2/Models.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc2
+{
+    public enum EnumPeriod
+    {
+        Bulanan = 1,
+        Triwulanan = 3,
+        Semesteran = 6,
+        Tahunan = 12,
+        JatuhTempo = 0
+    }
+
+    public enum EnumTransactionType
+    {
+        Pencairan = 0,
+        PembayaranAngsuran = 1,
+        PembayaranTunggakan = 2,
+        PembayaranPokok = 3,
+        PembayaranBunga = 4
+    }
+
+    public class Collect
+    {
+        public int Status { get; set; }
+        public int MinDpd { get; set; }
+        public int MaxDpd { get; set; }
+
+        public Collect(int status, int minDpd, int maxDpd) 
+        {
+            this.Status = status;
+            this.MinDpd = minDpd;
+            this.MaxDpd = maxDpd;
+        }
+    }
+
+    /*
+    public static List<Collect> Collectability = new List<Collect>
+    {
+        new Collect(1, 0, 10),
+        new Collect(2, 11, 90),
+        new Collect(3, 91, 120),
+        new Collect(4, 121, 180),
+        new Collect(5, 181, 99999)
+    };
+    */
+
+    public class Facility
+    {
+        public int Id { get; set; }
+        public double Limit { get; set; }
+        public int Tenor { get; set; }
+        public double InterestRate { get; set; }
+        public double DendaRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime MaturityDate { get; set; }
+        public bool? IsRevolving { get; set; }
+        public EnumPeriod? PeriodePerhitunganPokok { get; set; }
+        public EnumPeriod? PeriodePerhitunganBunga { get; set; }
+        public double? BakiDebet { get; set; }
+        public double? AvailableLimit { get; set; }
+        public double? TotalPokok { get; set; }
+        public double? TotalBunga { get; set; }
+        public double? TotalDendaPokok { get; set; }
+        public double? TotalDendaBunga { get; set; }
+        public double? TotalLainnya { get; set; }
+        public double? TotalKewajiban { get; set; }
+        public int? Dpd { get; set; }
+        public int? Kolektibilitas { get; set; }
+        public int TanggalCutOff { get; set; }
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int FacilityId { get; set; }
+        public EnumTransactionType TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public double TransactionAmount { get; set; }
+
+        public Transaction(int id, int facilityId, EnumTransactionType transactionType, DateTime transactionDate, double transactionAmount)
+        {
+            this.Id = id;
+            this.FacilityId = facilityId;
+            this.TransactionType = transactionType;
+            this.TransactionDate = transactionDate;
+            this.TransactionAmount = transactionAmount;
+        }
+    }
+
+    public class Billing
+    {
+        public int Id { get; set; }
+        public int FacilityId { get; set; }
+        public int? PeriodeKe { get; set; }
+        public DateTime DueDate { get; set; }
+        public double? Pokok { get; set; }
+        public double? Bunga { get; set; }
+        public double? DendaPokok { get; set; }
+        public double? DendaBunga { get; set; }
+        public double? Lainnya { get; set; }
+        public int? Dpd { get; set; }
+        public bool? IsPaid { get; set; }
+        public DateTime? PaidDate { get; set; }
+        public double? PaidAmount { get; set; }
+        public int? TransactionId { get; set; }
+    }
+
+    public class PosisiBakiDebet
+    {
+        public long Id { get; set; }
+        public int PeriodeKe { get; set; }
+        public DateTime MulaiBakiDebet { get; set; }
+        public DateTime AkhirBakiDebet { get; set; }
+        public int JumlahHariBakiDebet { get; set; }
+        public double BakiDebet { get; set; }
+        public double Bunga { get; set; }
+        public double JumlahBunga { get; set; }
+    }
+}
---
+++ calc2/Program.cs
@@ -0,0 +1,138 @@
+﻿using calc2;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                .AddConsole();
+        });
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+        logger.LogInformation("Program is starting...");
+
+        /************************/
+        /*** Set-Up Fasilitas ***/
+        /************************/
+
+        Facility facility = new Facility();
+        facility.Id = 1;
+        facility.Limit = 10000000;
+        facility.Tenor = 12;
+        facility.InterestRate = 12;
+        facility.StartDate = new DateTime(2022, 1, 1);
+        facility.MaturityDate = new DateTime(2023, 1, 1);
+        facility.IsRevolving = true;
+        facility.PeriodePerhitunganBunga = EnumPeriod.Triwulanan;
+        facility.PeriodePerhitunganPokok = EnumPeriod.Triwulanan;
+        facility.TanggalCutOff = 31;
+
+        /************************/
+        /*** Set-Up Transaksi ***/
+        /************************/
+
+        List<Transaction> listTransaction = new List<Transaction>()
+        {
+            new Transaction(1, 1, EnumTransactionType.Pencairan, new DateTime(2022, 1, 16), 3000000),
+            new Transaction(2, 1, EnumTransactionType.Pencairan, new DateTime(2022, 3, 16), 2000000),
+            new Transaction(3, 1, EnumTransactionType.Pencairan, new DateTime(2022, 5, 16), 1000000)
+        };
+
+        /******************/
+        /*** Calulation ***/
+        /******************/
+
+        // Array of Due-Date
+        DateTime[] arrDueDate = new DateTime[1];
+        // Due-Date[0] => Tanggal Mulai
+        arrDueDate[0] = facility.StartDate;
+
+        if ((facility.TanggalCutOff < 1) || (facility.TanggalCutOff > 31))
+        {
+            throw new Exception("Tanggal Cut-Off tidak valid!");
+        }
+
+        // Due-Date[1] s/d Due-Date[N-1]
+        DateTime nextDueDate;
+        int n = 0, nextYear, nextMonth;
+        int s = (int)facility.PeriodePerhitunganBunga;
+        try
+        {
+            nextYear = arrDueDate[0].Year;
+            nextMonth = arrDueDate[0].Month;
+
+            nextDueDate = new DateTime(nextYear, nextMonth,
+                    (facility.TanggalCutOff > DateTime.DaysInMonth(nextYear, nextMonth)) ? DateTime.DaysInMonth(nextYear, nextMonth) : facility.TanggalCutOff);
+
+            if (nextDueDate <= arrDueDate[0])
+            {
+                nextMonth = nextMonth + s;
+                if (nextMonth > 12)
+                {
+                    nextYear++;
+                    nextMonth = nextMonth % 12;
+                }
+
+                nextDueDate = new DateTime(nextYear, nextMonth,
+                    (facility.TanggalCutOff > DateTime.DaysInMonth(nextYear, nextMonth)) ? DateTime.DaysInMonth(nextYear, nextMonth) : facility.TanggalCutOff);
+            }
+
+            while (nextDueDate < facility.MaturityDate)
+            {
+                if (n > 0)
+                {
+                    nextYear = nextDueDate.Year;
+                    nextMonth = nextDueDate.Month + s;
+                    if (nextMonth > 12)
+                    {
+                        nextYear++;
+                        nextMonth = nextMonth % 12;
+                    }
+
+                    logger.LogInformation("n={0}, y={1}, m={2}", n, nextYear, nextMonth);
+
+                    nextDueDate = new DateTime(nextYear, nextMonth,
+                    (facility.TanggalCutOff > DateTime.DaysInMonth(nextYear, nextMonth)) ? DateTime.DaysInMonth(nextYear, nextMonth) : facility.TanggalCutOff);
+                }
+
+                if (nextDueDate < facility.MaturityDate)
+                {
+                    n++;
+                    Array.Resize(ref arrDueDate, arrDueDate.Length + 1);
+                    arrDueDate[n] = nextDueDate;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e.Message);
+        }
+
+        // Due-Date[N] => Maturity Date
+        Array.Resize(ref arrDueDate, arrDueDate.Length + 1);
+        arrDueDate[arrDueDate.Length - 1] = facility.MaturityDate;
+
+        for (int i = 0; i < arrDueDate.Length - 1; i++)
+        {
+            logger.LogInformation("Calculation from: {0:d}, until: {1:d}", arrDueDate[i], arrDueDate[i+1]);
+
+            // Cari semua transaksi dalam periode ini
+            List<Transaction> listTransactionInPeriod = listTransaction
+                .Where(x => x.TransactionDate >= arrDueDate[i] && x.TransactionDate <= arrDueDate[i+1])
+                .ToList();
+
+            foreach (Transaction transaction in listTransactionInPeriod)
+            {
+                logger.LogInformation("Transaction Id: {0}, Date: {1:d}, Type: {2}, Amount: {3:C}", transaction.Id, transaction.TransactionDate, transaction.TransactionType, transaction.TransactionAmount);
+            }
+        }
+
+        logger.LogInformation("Program is completed!");
+    }
+}
